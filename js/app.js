ef9682652d276f7fc59b7a1bb164f05c1b25226a
Rok Pajnič <Rok Pajni>
2016-11-12 23:32:57 +0100

    Model movement, Assets added

--- js/app.js
+++ js/app.js
@@ -3,24 +3,97 @@
 var canvas = document.getElementById('canvas');
 var engine = new BABYLON.Engine(canvas, true);
 
+var actor = {
+        // Movement attributes
+        speed : 3,
+        moveLeft : false,
+        moveRight : false,
+        moveForward : false,
+        moveBackwards : false,
+        model: null
+        };
 
 var actor, camera;
+var charModel, asset;
+
+
+//  Register key presses
+var initMovement = function() {
+    //console.log("MOVING");
+
+    // When a key is pressed, set the movement
+    var onKeyDown = function(evt) {
+        // console.log(evt.keyCode);
+        // To the left
+        if (evt.keyCode == 65) {
+            actor.moveLeft = true;
+            actor.moveRight = false;
+        } 
+        else if (evt.keyCode == 68) {
+            // To the right
+            actor.moveRight = true;
+            actor.moveLeft = false;
+        }
+        else if (evt.keyCode == 87) {
+            // Forward
+            actor.moveForward = true;
+            actor.moveBackwards = false;
+        }
+        else if (evt.keyCode == 83) {
+            // Backwards
+            actor.moveBackwards = true;
+            actor.moveForward = false;
+        }
+    };
+
+    // On key up, reset the movement
+    var onKeyUp = function(evt) {
+        //console.log("up event: " + evt.keyCode);
+        if (evt.keyCode == 68) actor.moveRight = false;
+        if (evt.keyCode == 65) actor.moveLeft = false;
+        if (evt.keyCode == 87) actor.moveForward = false;
+        if (evt.keyCode == 83) actor.moveBackwards = false;
+    };
+
+    // Register events with the right Babylon function
+    BABYLON.Tools.RegisterTopRootEvents([{
+        name: "keydown",
+        handler: onKeyDown
+    }, {
+        name: "keyup",
+        handler: onKeyUp
+    }]);
+};
+
+//Movement
+var move = function() {
+    var pos = actor.model.position;
+    if (actor.moveRight) {
+        pos.x += -0.3;
+        //camera.position.x += 0.3;
+    }
+    if (actor.moveLeft) {
+        pos.x += +0.3;
+        //camera.position.x += -0.3;
+    }
+    if (actor.moveForward) {
+        pos.z += -0.3;
+        //camera.position.z += 0.3;
+    }
+    if (actor.moveBackwards) {
+        pos.z += +0.3;
+        //camera.position.z += -0.3;
+    }
+};
+
 
 var createScene = function() {
     // create a basic BJS Scene object
     var scene = new BABYLON.Scene(engine);
 
     var camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 50, 17), scene);
-    /*
-    // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-    camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 15,-17), scene);
-    camera.speed = 4;
-    // target the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
 
-    // attach the camera to the canvas
-    camera.attachControl(canvas, false);
-    */
+    console.log(camera);
     // create a basic light, aiming 0,1,0 - meaning, to the sky
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
@@ -29,36 +102,85 @@ var createScene = function() {
 
     //Creation of a repeated textured material
     var materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
-    materialPlane.diffuseTexture = new BABYLON.Texture("textures/ground.jpg", scene);
+    materialPlane.diffuseTexture = new BABYLON.Texture("textures/Plate_Floor_1_D.jpg", scene);
     materialPlane.diffuseTexture.uScale = 55.0;//Repeat 5 times on the Vertical Axes
     materialPlane.diffuseTexture.vScale = 55.0;//Repeat 5 times on the Horizontal Axes
     materialPlane.backFaceCulling = false;//Always show the front and the back of an element
-
     ground.material = materialPlane;
 
-    actor = new Actor(1, scene);
+    initMovement();
 
-    camera.target = actor;
-    camera.radius = 15;
-    camera.heightOffset = 8;
-    // return the created scene
+    // The function ImportMesh will import our custom model in the scene given in parameter
+    var _this  = this;
     
+        // The loader
+    var loader =  new BABYLON.AssetsManager(scene);
+
+
+    BABYLON.SceneLoader.Load("./assets/Varian/", "psc-warrior.babylon", engine, function (newScene) {
+            // Wait for textures and shaders to be ready
+            console.log("LOADED");
+            console.log(newScene);
+            newScene.executeWhenReady(function () {
+                // Attach camera to canvas inputs
+                newScene.activeCamera.attachControl(canvas);
+
+                // Once the scene is loaded, just register a render loop to render it
+                engine.runRenderLoop(function() {
+                    newScene.render();
+                });
+            });
+        }, function (progress) {
+            // To do: give progress feedback to user
+        });
+
+
+    var model = loader.addMeshTask("elf", "", "./assets/Varian/", "psc-warrior.babylon");
+    model.onSuccess = function(t) {
+        console.log(t);
+        actor.model = new BABYLON.Mesh("snowman", _this.scene);
+        t.loadedMeshes.forEach(function(m) {
+            m.parent = actor.model;
+        });
+        actor.model.scaling.scaleInPlace(1);
+        //actor.model.rotation.y = -Math.PI/2;
+        //actor.model.position.y = 0.5;
+        actor.model.setEnabled(true);
+        asset = {meshes: actor.model};
+
+        console.log("Actor");
+
+        camera.target = actor.model;
+        camera.radius = 15;
+        camera.heightOffset = 8;
+        camera.rotationOffset = 0; // the viewing angle
+        
+        engine.runRenderLoop(function() {
+            if (!actor.killed) {
+                move();
+                //snowman.position.x += 0.1;
+                /*camera.position.z += actor.speed;
+                actor.position.z += actor.speed;
+                ground.position.z += actor.speed;*/
+                
+            }
+            scene.render();
+        });
+    };
+
+    loader.load();
+
+
+    
+
+    // return the created scene
+
     return scene;
 }
 
 var scene = createScene();
 
-engine.runRenderLoop(function() {
-    if (!actor.killed) {
-        actor.move();
 
-        /*camera.position.z += actor.speed;
-        actor.position.z += actor.speed;
-        ground.position.z += actor.speed;*/
-        
-    }
-    scene.render();
-});
 
 window.addEventListener('resize', function() {
     engine.resize();
