0aa59511bd2d9ae3e1fbd5fe54bcf0ce1489e9e4
Rok Pajnič <Rok Pajni>
2017-01-04 22:52:41 +0100

    Weapon assets added

---
+++ TheKabundaUE/Source/TheKabundaUE/Pickup.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class THEKABUNDAUE_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+	
+	// Static Mesh for the pickup
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* SM_Pickup;
+	
+	
+};
