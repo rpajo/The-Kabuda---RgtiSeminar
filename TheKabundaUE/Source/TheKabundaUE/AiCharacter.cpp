e3c09d947a572b24640e7bc935f0c2d710e8b76d
Rok Pajnič <Rok Pajni>
2017-01-06 14:01:19 +0100

    Enemy class and AI

---
+++ TheKabundaUE/Source/TheKabundaUE/AiCharacter.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheKabundaUE.h"
+#include "AiCharacter.h"
+
+
+// Sets default values
+AAiCharacter::AAiCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAiCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAiCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AAiCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
---
+++ TheKabundaUE/Source/TheKabundaUE/AiControllerClass.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheKabundaUE.h"
+#include "AiControllerClass.h"
+
+
+
+
